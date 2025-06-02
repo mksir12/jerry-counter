d9808b2c4ba668edaeb489562e1942ffac407745
mksir12 <116490353+mksir12@users.noreply.github.com>
2025-06-02 22:42:29 +0530

    Delete api/get.js

--- api/get.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs/promises';
-import path from 'path';
-
-export default async function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'counter.json');
-  const data = JSON.parse(await fs.readFile(filePath, 'utf-8'));
-  return res.status(200).json({ count: data.count });
-}
